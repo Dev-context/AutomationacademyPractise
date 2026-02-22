72ba949c37c3ed2376a97f94a2e7f8e5b49f2b4b
Dev-Context <vinicius.smtp.cmd@gmail.com>
2026-02-22 15:48:43 +0000

    Enhance Test Reporting, Setup POM, and Configure Code Quality Tools

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import { testeBase as test, expect } from "../fixtures/base.fixture.spec.ts";
+import { testeBase as test } from "../fixtures/base.fixture.spec.ts";
 import { ENV } from "../config/env";
 import LoginPage from "../model/login/LoginPage";
 
@@ -27,11 +27,5 @@ test.describe("Login Suite", () => {
   test("@CT004 Unsuccessfully logged in with invalid email format", async () => {
     await loginPage.goTo();
     await loginPage.login("invalidemail.com", ENV.USERPASSWORD, false);
-  });
-
-  test("DEBUG - Intentional Failure for Reporting", async ({ page }) => {
-    await loginPage.goTo();
-    // This will fail because this text does not exist on the login page
-    await expect(page.getByText("THIS TEXT DOES NOT EXIST - TO TEST REPORTING")).toBeVisible({ timeout: 5000 });
   });
 });
